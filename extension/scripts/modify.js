0a29bbca231bb8b561803307a029629c900458e2
yury.kosharovskiy <kosharovskiy@gmail.com>
2025-02-06 21:06:11 +0200

    Update manifest to allow all URLs and enhance project management script with storage integration

--- extension/scripts/modify.js
+++ extension/scripts/modify.js
@@ -1,9 +1,32 @@
-const projects = {
-    'projectId': 'projectName'
-};
+let projects = {};
+
+// Load initial projects from storage
+chrome.storage.sync.get(['projects'], (result) => {
+    projects = {};
+    (result.projects || []).forEach(p => {
+        projects[p.id] = p.name;
+    });
+});
+
+// Listen for changes in storage
+chrome.storage.onChanged.addListener((changes, namespace) => {
+    console.error('Storage changed', changes, namespace);
+    if (namespace === 'sync' && changes.projects) {
+        projects = {};
+        changes.projects.newValue.forEach(p => {
+            projects[p.id] = p.name;
+        });
+
+        updateProject(projects);
+    }
+});
 
 setInterval(() => {
-    let dropDownMenu = document.querySelectorAll('button.co-namespace-dropdown__menu-toggle > span.pf-c-menu-toggle__text');
+    updateProject(projects);
+}, 1000)
+
+const updateProject = (projects) => {
+    let dropDownMenu = document.querySelectorAll('button.co-namespace-dropdown__menu-toggle > span.pf-v5-c-menu-toggle__text');
 
     if (dropDownMenu.length > 0) {
         let text = dropDownMenu[0].innerHTML;
@@ -15,8 +38,8 @@ setInterval(() => {
         }
     }
 
-    let dropDownMenuItems = document.querySelectorAll('ul.pf-c-menu__list > li.pf-c-menu__list-item > button.pf-c-menu__item > span > span');
-
+    let dropDownMenuItems = document.querySelectorAll('ul.pf-v5-c-menu__list > li.pf-v5-c-menu__list-item > button.pf-v5-c-menu__item > span > span');
+    
     if (dropDownMenuItems.length > 0) {
         if (dropDownMenuItems.length > 0) {
             dropDownMenuItems.forEach(item => {
@@ -27,4 +50,4 @@ setInterval(() => {
             });
         }
     }
-}, 1000)
+}
